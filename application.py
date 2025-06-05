a025f1166e1822d49df19ccd8c7a968029abd136
Divyansh Gupta <divyansh_2301me17@iitp.ac.in>
2025-06-05 19:51:46 +0530

    Deployment Configuration

--- application.py
+++ application.py
@@ -37,4 +37,4 @@ def predict_datapoints():
         return render_template('home_enhanced.html', results=res[0])
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0", debug = True, port = 5000)    
+    app.run(host = "0.0.0.0", port = 5000)    
